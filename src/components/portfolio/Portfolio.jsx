be5d58966e3126906c986a8b8a7fb725f5ae29ac
bimochanb <bimochanwhive@gmail.com>
2024-03-19 18:45:54 +0545

    worked on the portfolio and restimonials section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,11 +1,84 @@
 import React from 'react';
 import './Portfolio.css';
+import IMG1 from '../../assets/images/portfolio1.jpg';
+import IMG2 from '../../assets/images/portfolio2.jpg';
+import IMG3 from '../../assets/images/portfolio3.jpg';
+import IMG4 from '../../assets/images/portfolio4.jpg';
+import IMG5 from '../../assets/images/portfolio6.jpg';
+import IMG6 from '../../assets/images/portfolio5.png';
+
+const data = [
+    {
+        id: 1,
+        title: "QuickCare",
+        image: IMG1,
+        description: "Doctor Consultation Application",
+        github: "https://github.com/BimoChan07/QuickCare-PHP"
+    },
+    {
+        id: 2,
+        title: "KisanArea",
+        image: IMG2,
+        description: "Farmers Assistance Web Application",
+        github: "https://github.com/BimoChan07/KisanArea"
+    },
+    {
+        id: 3,
+        title: "Strumo",
+        image: IMG3,
+        description: "Online Guitar Store",
+        github: "https://github.com/BimoChan07/Strumo"
+    },
+    {
+        id: 4,
+        title: "Nepal Life Capital",
+        image: IMG4,
+        description: "NLC",
+        github: "/project2"
+    },
+    {
+        id: 5,
+        title: "Hospital Management System",
+        image: IMG5,
+        description: "HMS",
+        github: "https://github.com/BimoChan07/hms"
+    },
+    {
+        id: 5,
+        title: "Sawari Rental",
+        image: IMG6,
+        description: "Sawari Car Rental",
+        github: "https://github.com/BimoChan07/sawari"
+    }
+];
+
 
 const Portfolio = () => {
     return (
         <section id='portfolio'>
+            <h5>My Recent Work</h5>
+            <h2>Portfolio</h2>
+            <div className="container portfolio__container">
+                {
+                    data.map(({ id, image, title, description, github }) => {
+                        return (
+                            < article key={id} className="portfolio__item" >
+                                <div className="portfolio__item-img">
+                                    <img src={image} alt={title} />
+                                </div>
+                                <h3>{title}</h3>
+                                <p>{description}</p>
+                                <div className="portfolio__item-cta">
+                                    <a href={github} className='btn'>Github</a>
+                                    <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
+                                </div>
+                            </article>
+                        )
+                    })
+                }
 
-        </section>
+            </div>
+        </section >
     );
 }
 
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,10 +1,67 @@
 import React from 'react';
 import './Testimonials.css';
+import AVTR1 from '../../assets/images/avatar1.jpg'
+import AVTR2 from '../../assets/images/avatar2.jpg'
+import AVTR3 from '../../assets/images/avatar3.jpg'
+import AVTR4 from '../../assets/images/avatar4.jpg'
+import { Pagination } from 'swiper/modules';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+const data = [
+    {
+        avatar: AVTR2,
+        name: 'Roshan Neupane',
+        review: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi assumenda impedit ipsum ad inventore aliquam ex, error maiores cum sequi?'
+    },
+
+    {
+        avatar: AVTR3,
+        name: 'Nischal Dahal',
+        review: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi assumenda impedit ipsum ad inventore aliquam ex, error maiores cum sequi?'
+    },
+    {
+        avatar: AVTR1,
+        name: 'Alisha BB Shrestha',
+        review: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi assumenda impedit ipsum ad inventore aliquam ex, error maiores cum sequi?'
+    },
+    {
+        avatar: AVTR4,
+        name: 'Subash Rai',
+        review: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi assumenda impedit ipsum ad inventore aliquam ex, error maiores cum sequi?'
+    },
+]
 
 const Testimonials = () => {
     return (
         <section id='testimonials'>
+            <h5>Review from clients</h5>
+            <h2>Testimonials</h2>
+            <Swiper className="container testimonials__container"
+                modules={[Pagination]}
+                spaceBetween={40}
+                slidesPerView={1}
+                pagination={{ clickable: true }}
+            >
+                {
+                    data.map(({ avatar, name, review }, index) => {
+                        return (
 
+                            <SwiperSlide key={index} className="testimonial">
+                                <div className="client__avatar">
+                                    <img src={avatar} alt={name} />
+                                </div>
+                                <h5 className='client__name'>{name}</h5>
+                                <small className="client__review">{review}</small>
+                            </SwiperSlide>
+                        )
+                    })
+                }
+            </Swiper>
         </section>
     );
 }
